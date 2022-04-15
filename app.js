ffe5ea8975216a35d4817e0984a4fb5fe86c1eb7
Vijendra Saini <docs.vijendra@gmail.com>
2022-04-15 23:35:41 +0000

    pushing code ..

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var userprofileController = require('./controllers/userprofile.controller')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,8 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+app.use('/', userprofileController);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
